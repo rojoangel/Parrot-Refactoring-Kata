429e882613db06663d77c695526cc5019978dc71
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-07-19 22:55:07 +0200

    european parrot implements interface

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,15 +1,6 @@
 package parrot;
 
-class EuropeanParrot extends Parrot {
-
-
-    private static final int NUMBER_OF_COCONUTS = 0;
-    private static final int VOLTAGE = 0;
-    private static final boolean IS_NAILED = false;
-
-    public EuropeanParrot() {
-        super(ParrotTypeEnum.EUROPEAN, VOLTAGE, IS_NAILED);
-    }
+class EuropeanParrot implements ParrotInterface {
 
     public double getSpeed() {
         return 12.0;
