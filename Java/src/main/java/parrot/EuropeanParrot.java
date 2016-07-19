f253f25fcf7dd558b071cf8cfc029e233ad59cf9
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-07-19 21:54:42 +0200

    parrot factory creates european parrot

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,6 +1,16 @@
 package parrot;
 
-class EuropeanParrot {
+class EuropeanParrot extends Parrot {
+
+
+    public static final int NUMBER_OF_COCONUTS = 0;
+    public static final int VOLTAGE = 0;
+    public static final boolean IS_NAILED = false;
+
+    public EuropeanParrot() {
+        super(ParrotTypeEnum.EUROPEAN, NUMBER_OF_COCONUTS, VOLTAGE, IS_NAILED);
+    }
+
     public double getSpeed() {
         return 12.0;
     }
--- Java/src/main/java/parrot/ParrotFactory.java
+++ Java/src/main/java/parrot/ParrotFactory.java
@@ -4,6 +4,11 @@ public class ParrotFactory {
 
     public static Parrot create(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed)
     {
-        return new Parrot(type, numberOfCoconuts, voltage, isNailed);
+        switch (type) {
+            case EUROPEAN:
+                return new EuropeanParrot();
+            default:
+                return new Parrot(type, numberOfCoconuts, voltage, isNailed);
+        }
     }
 }
