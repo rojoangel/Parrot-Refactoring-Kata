e523a06e7abdf1388066b093079b74843c5c7839
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-07-19 21:58:17 +0200

    refactor: reduce european parrot constant visibility

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -3,9 +3,9 @@ package parrot;
 class EuropeanParrot extends Parrot {
 
 
-    public static final int NUMBER_OF_COCONUTS = 0;
-    public static final int VOLTAGE = 0;
-    public static final boolean IS_NAILED = false;
+    private static final int NUMBER_OF_COCONUTS = 0;
+    private static final int VOLTAGE = 0;
+    private static final boolean IS_NAILED = false;
 
     public EuropeanParrot() {
         super(ParrotTypeEnum.EUROPEAN, NUMBER_OF_COCONUTS, VOLTAGE, IS_NAILED);
